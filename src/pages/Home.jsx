794f2a68996f6452e3fe6087526cb129130f9f5f
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-05-24 00:46:11 +0530

    created responsive navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
+import { StarBackground } from '../components/StarBackground'
+import Navbar from '../components/Navbar'
+
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
 
       <ThemeToggle/>
+      <StarBackground/>
+      <Navbar/>
       
     </div>
   )
